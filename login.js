8bdf9b11f561ed9075f177728b89acd88333d203
Jenish Limbu <jlimbu539@gmail.com>
2024-04-26 16:02:58 +0545

    login and registration

--- login.js
+++ login.js
@@ -1,4 +1,26 @@
 function login() {
   event.preventDefault();
-  window.location.href = "./index.html";
+  const emailPhone = document.getElementById("emailUserPhone").value;
+  const password = document.getElementById("password").value;
+  const getemailPhone = localStorage.getItem("userEmailPhone");
+  const getUserName = localStorage.getItem("userName");
+  const getPassword = localStorage.getItem("userPassword");
+  const emailPhoneName = [];
+  emailPhoneName.push(getemailPhone);
+  emailPhoneName.push(getUserName);
+  if (emailPhoneName.includes(emailPhone) && password == getPassword) {
+    sessionStorage.setItem("loggedIn", "true");
+    window.location.href = "./index.html";
+  }
 }
+
+document.getElementById("showHide").addEventListener("click", function () {
+  const password = document.getElementById("password");
+  if (password.type == "password") {
+    password.type = "text";
+    this.innerHTML = "Hide";
+  } else {
+    password.type = "password";
+    this.innerHTML = "Show";
+  }
+});
--- registration.js
+++ registration.js
@@ -1,4 +1,77 @@
+const emailRegex =
+  /^[a-zA-Z0-9_\. \-]+\@[a-zA-Z0-9\-]+\.+[a-zA-Z0-9]{2,4}|(^[0-9]{10})+$/;
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+const usernameReg = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
+
 function register() {
   event.preventDefault();
-  window.location.href = "./login.html";
+  const fullName = document.getElementById("fullName").value;
+  const userName = document.getElementById("userName").value;
+  const emailPhone = document.getElementById("emailOrMobile").value;
+  const password = document.getElementById("password").value;
+  if (
+    emailRegex.test(emailPhone) &&
+    passwordRegex.test(password) &&
+    usernameReg.test(userName) &&
+    fullName.length > 0
+  ) {
+    localStorage.setItem("userEmailPhone", emailPhone);
+    localStorage.setItem("userPassword", password);
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("fullName", fullName);
+    window.location.href = "./login.html";
+  }
 }
+function validateEmailPhone() {
+  const iconSrc = document.getElementById("email-validation-icon");
+  const emailPhone = document.getElementById("emailOrMobile").value;
+  if (!emailRegex.test(emailPhone)) {
+    iconSrc.src = "./icons/close.png";
+    iconSrc.style.display = "block";
+  } else {
+    iconSrc.src = "./icons/check-mark.png";
+    iconSrc.style.display = "block";
+  }
+}
+function validatePassword() {
+  const iconSrc = document.getElementById("password-validation-icon");
+  const password = document.getElementById("password").value;
+  if (!passwordRegex.test(password)) {
+    iconSrc.src = "./icons/close.png";
+    iconSrc.style.display = "block";
+  } else {
+    iconSrc.src = "./icons/check-mark.png";
+    iconSrc.style.display = "block";
+  }
+}
+const fullNameIcon = document.getElementById("fullName-validation-icon");
+function validateFullName() {
+  const fullName = document.getElementById("fullName").value;
+  if (fullName.length > 0) {
+    fullNameIcon.style.display = "block";
+  } else {
+    fullNameIcon.style.display = "none";
+  }
+}
+function validateUsername() {
+  const userName = document.getElementById("userName").value;
+  const validateIcon = document.getElementById("username-validation-icon");
+  if (!usernameReg.test(userName)) {
+    validateIcon.src = "./icons/close.png";
+    validateIcon.style.display = "block";
+  } else {
+    validateIcon.src = "./icons/check-mark.png";
+    validateIcon.style.display = "block";
+  }
+}
+document.getElementById("showHide").addEventListener("click", function () {
+  const password = document.getElementById("password");
+  if (password.type == "password") {
+    password.type = "text";
+    this.innerHTML = "Hide";
+  } else {
+    password.type = "password";
+    this.innerHTML = "Show";
+  }
+});
